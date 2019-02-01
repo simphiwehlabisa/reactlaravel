464a0eb104a973468831c87ac1749692e732c5ab
simphiwe sifiso hlabisa <sfisohlabisa@gmail.com>
2019-02-01 13:17:19 +0200

    initial

---
+++ resources/js/components/App.js
@@ -0,0 +1,54 @@
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+
+// export default class Example extends Component {
+//     render() {
+//         return (
+//             <div className="container">
+//                 <div className="row justify-content-center">
+//                     <div className="col-md-8">
+//                         <div className="card">
+//                             <div className="card-header">Example Component</div>
+
+//                             <div className="card-body">
+//                                 I'm an example component!
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+// if (document.getElementById('example')) {
+//     ReactDOM.render(<Example />, document.getElementById('example'));
+// }
+
+
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Header from './Header'
+import NewProject from './NewProject'
+import ProjectsList from './ProjectsList'
+import SingleProject from './SingleProject'
+class App extends Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={ProjectsList} />
+            <Route path='/create' component={NewProject} />
+            <Route path='/:id' component={SingleProject} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
